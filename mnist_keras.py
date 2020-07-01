0bcf8df6e7d76e593295e6443c116e6fd091c081
Andiry Xu <andiryxu@google.com>
2020-06-30 23:50:03 -0700

    Add Conv2D model

--- mnist_keras.py
+++ mnist_keras.py
@@ -9,7 +9,7 @@ from tensorflow import keras
 from tensorflow.keras import datasets,optimizers,layers
 
 
-class mnist_model(object):
+class mnist_dense_model(object):
   def __init__(self):
     self.network = keras.Sequential([
         layers.Dense(256, activation='relu'),
@@ -23,8 +23,33 @@ class mnist_model(object):
                          metrics=['accuracy'])
 
 
-def create_db(x, y):
-  def preprocess(x, y):
+class mnist_conv2d_model(object):
+  def __init__(self):
+    self.network = keras.Sequential([
+        layers.Conv2D(6, kernel_size=3, strides=1),
+        layers.MaxPooling2D(pool_size=2, strides=2),
+        layers.ReLU(),
+        layers.Conv2D(16, kernel_size=3, strides=1),
+        layers.MaxPooling2D(pool_size=2, strides=2),
+        layers.ReLU(),
+        layers.Flatten(),
+        layers.Dense(120, activation='relu'),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(10)])
+    self.network.compile(optimizer=optimizers.Adam(lr=0.01),
+                         loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+                         metrics=['accuracy'])
+
+
+def create_db(x, y, using_conv2d):
+  def preprocess_conv2d(x, y):
+    x = tf.cast(x, dtype=tf.float32) / 255.0
+    x = tf.expand_dims(x, axis=3)
+    y = tf.cast(y, dtype=tf.int32)
+    y = tf.one_hot(y, depth=10)
+    return x, y
+
+  def preprocess_dense(x, y):
     x = tf.cast(x, dtype=tf.float32) / 255.0
     x = tf.reshape(x, [-1, 784])
     y = tf.cast(y, dtype=tf.int32)
@@ -33,12 +58,19 @@ def create_db(x, y):
 
   db = tf.data.Dataset.from_tensor_slices((x, y))
   db = db.shuffle(10000).batch(128)
-  db = db.map(preprocess)
+  if using_conv2d:
+    db = db.map(preprocess_conv2d)
+  else:
+    db = db.map(preprocess_dense)
+
   return db
 
 
-def train(train_db, eval_db):
-  model = mnist_model()
+def train(train_db, eval_db, using_conv2d):
+  if using_conv2d:
+    model = mnist_conv2d_model()
+  else:
+    model = mnist_dense_model()
   history = model.network.fit(train_db, epochs=10, validation_data=eval_db,
                               validation_freq=5)
   print(model.network.summary())
@@ -50,10 +82,11 @@ def main():
   (x, y), (x_test, y_test) = datasets.mnist.load_data()
   print('x:', x.shape, 'y:', y.shape, 'x test:', x_test.shape, 'y test:', y_test)
 
-  train_db = create_db(x, y)
-  eval_db = create_db(x_test, y_test)
+  using_conv2d = True
+  train_db = create_db(x, y, using_conv2d=using_conv2d)
+  eval_db = create_db(x_test, y_test, using_conv2d=using_conv2d)
 
-  train(train_db, eval_db)
+  train(train_db, eval_db, using_conv2d=using_conv2d)
 
 
 if __name__ == '__main__':
