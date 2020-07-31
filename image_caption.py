062c24660ffcd2d09ba4dc58073a530acb40866c
Andiry Xu <andiryxu@google.com>
2020-07-31 17:04:33 +0000

    Image caption

---
+++ image_caption.py
@@ -0,0 +1,351 @@
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+import re
+import numpy as np
+import os
+import time
+import json
+from glob import glob
+from PIL import Image
+import pickle
+
+# Download caption annotation files
+annotation_folder = '/annotations/'
+if not os.path.exists(os.path.abspath('.') + annotation_folder):
+    annotation_zip = tf.keras.utils.get_file('captions.zip',
+                                            cache_subdir=os.path.abspath('.'),
+                                            origin = 'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+                                            extract = True)
+    annotation_file = os.path.dirname(annotation_zip) + '/annotations/captions_train2014.json'
+    os.remove(annotation_zip)
+else:
+    annotation_file = os.path.abspath('./annotations/captions_train2014.json') 
+
+
+# Download image files
+image_folder = '/train2014/'
+if not os.path.exists(os.path.abspath('.') + image_folder):
+    image_zip = tf.keras.utils.get_file('train2014.zip',
+                                       cache_subdir=os.path.abspath('.'),
+                                       origin = 'http://images.cocodataset.org/zips/train2014.zip',
+                                       extract = True)
+    PATH = os.path.dirname(image_zip) + image_folder
+    os.remove(image_zip)
+else:
+    PATH = os.path.abspath('.') + image_folder
+
+with open(annotation_file, 'r') as f:
+    annotations = json.load(f)
+
+# Store captions and image names in vectors
+all_captions = []
+all_image_name_vector = []
+
+for annotation in annotations['annotations']:
+    caption = '<start> ' + annotation['caption'] + ' <end>'
+    image_id = annotation['image_id']
+    full_coco_image_path = PATH + 'COCO_train2014_' + '%012d.jpg' % (image_id)
+
+    all_image_name_vector.append(full_coco_image_path)
+    all_captions.append(caption)
+
+# Shuffle captions and image_names
+train_captions, image_name_vector = shuffle(all_captions, all_image_name_vector, random_state=1)
+
+num_examples = 100000
+train_captions = train_captions[:num_examples]
+image_name_vector = image_name_vector[:num_examples]
+
+print(len(train_captions), len(all_captions))
+
+def load_image(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (299, 299))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return img, image_path
+
+image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
+new_input = image_model.input
+hidden_layer = image_model.layers[-1].output
+
+image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+
+# Get unique images
+encode_train = sorted(set(image_name_vector))
+
+image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
+image_dataset = image_dataset.map(load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(64)
+
+for img, path in image_dataset:
+    batch_features = image_features_extract_model(img)
+    batch_features = tf.reshape(batch_features,
+                                (batch_features.shape[0], -1, batch_features.shape[3]))
+    
+    for bf, p in zip(batch_features, path):
+        path_of_feature = p.numpy().decode('utf-8')
+        np.save(path_of_feature, bf.numpy())
+
+# Find the maximum length of any caption in our dataset
+def calc_max_length(tensor):
+    return max(len(t) for t in tensor)
+
+# Choose top 10k words from vocabulary
+top_k = 10000
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k, oov_token='<unk>', filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+
+tokenizer.fit_on_texts(train_captions)
+train_seqs = tokenizer.texts_to_sequences(train_captions)
+
+tokenizer.word_index['<pad>'] = 0
+tokenizer.index_word[0] = '<pad>'
+
+# Create the tokenized vectors
+train_seqs = tokenizer.texts_to_sequences(train_captions)
+
+# Pad each vector to the max_length of the captions
+# If you do not provide a max_length value, pad_sequences calculates it automatically
+cap_vector = tf.keras.preprocessing.sequence.pad_sequences(train_seqs, padding='post')
+
+# Calculates the max_length, which is used to store the attention weights
+max_length = calc_max_length(train_seqs)
+
+# Create training and validation sets using an 80-20 split
+img_name_train, img_name_val, cap_train, cap_val = train_test_split(image_name_vector,
+                                                                    cap_vector,
+                                                                    test_size=0.2,
+                                                                    random_state=0)
+
+print(len(img_name_train), len(cap_train), len(img_name_val), len(cap_val))
+
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+embedding_dim = 256
+units = 512
+vocab_size = top_k + 1
+num_steps = len(img_name_train) // BATCH_SIZE
+# Shape of the vector extracted from InceptionV3 is (64, 2048)
+# These two variables represent that vector shape
+features_shape = 2048
+attention_features_shape = 64
+
+# Load the numpy files
+def map_func(img_name, cap):
+    img_tensor = np.load(img_name.decode('utf-8')+'.npy')
+    return img_tensor, cap
+
+dataset = tf.data.Dataset.from_tensor_slices((img_name_train, cap_train))
+
+# Use map to load the numpy files in parallel
+dataset = dataset.map(lambda item1, item2: tf.numpy_function(
+          map_func, [item1, item2], [tf.float32, tf.int32]),
+          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+# Shuffle and batch
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+
+class Attention(tf.keras.Model):
+    def __init__(self, units):
+        super().__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, features, hidden):
+        # Features (CNN encoder output) shape = (bs, 64, embedding_dim)
+        # Hidden shape = (bs, hidden_size)
+        # hidden_with_time_axis = (bs, 1, hidden_size)
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+
+        # Score shape = (bs, 64, hidden_size)
+        score = tf.nn.tanh(self.W1(features) + self.W2(hidden_with_time_axis))
+
+        # attention_weights shape = (bs, 64, 1)
+        attention_weights = tf.nn.softmax(self.V(score), axis=1)
+
+        # context_vector shape = (bs, hidden_size)
+        context_vector = attention_weights * features
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+
+class CNN_Encoder(tf.keras.Model):
+    def __init__(self, embedding_dim):
+        super().__init__()
+        # shape after = (bs, 64, embedding_dim)
+        self.fc = tf.keras.layers.Dense(embedding_dim)
+
+    def call(self, x):
+        return tf.nn.relu(self.fc(x))
+
+
+class CNN_Decoder(tf.keras.Model):
+    def __init__(self, embedding_dim, units, vocab_size):
+        super().__init__()
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.units, return_sequences=True, return_state=True,
+        recurrent_initializer='glorot_uniform')
+
+        self.fc1 = tf.keras.layers.Dense(self.units)
+        self.fc2 = tf.keras.layers.Dense(vocab_size)
+
+        self.attention = Attention(self.units)
+
+    def call(self, x, features, hidden):
+        context_vector, attention_weights = self.attention(features, hidden)
+
+        # x shape after embedding = (bs, 1, embedding_dim)
+        x = self.embedding(x)
+
+        # x shape after concat = (bs, 1, embedding_dim + hidden_size)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+        # Passing concate vector to GRU
+        output, state = self.gru(x)
+
+        # (bs, max_length, hidden_size)
+        x = self.fc1(output)
+
+        # (bs * max_length, hidden_size)
+        x = tf.reshape(x, (-1, x.shape[2]))
+
+        # (bs * max_length, vocab)
+        x = self.fc2(x)
+
+        return x, state, attention_weights
+
+    def reset_state(self, bs):
+        return tf.zeros((bs, self.units))
+
+
+encoder = CNN_Encoder(embedding_dim)
+decoder = CNN_Decoder(embedding_dim, units, vocab_size)
+
+
+optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+    return tf.reduce_mean(loss_)
+
+checkpoint_path = "./checkpoints/train"
+ckpt = tf.train.Checkpoint(encoder=encoder,
+                           decoder=decoder,
+                           optimizer = optimizer)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+start_epoch = 0
+if ckpt_manager.latest_checkpoint:
+    print('Restore checkpoint')
+    start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
+    # restoring the latest checkpoint in checkpoint_path
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+
+
+@tf.function
+def train_step(img_tensor, target):
+    loss = 0
+    hidden = decoder.reset_state(bs=target.shape[0])
+
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']] * target.shape[0], 1)
+
+    with tf.GradientTape() as tape:
+        features = encoder(img_tensor)
+
+        for i in range(1, target.shape[1]):
+            predictions, hidden, _ = decoder(dec_input, features, hidden)
+
+            loss += loss_function(target[:, i], predictions)
+
+            # Using teacher forcing
+            dec_input = tf.expand_dims(target[:, i], 1)
+
+        total_loss = loss /int(target.shape[1])
+
+        trainable_variables = encoder.trainable_variables + decoder.trainable_variables
+
+        gradients = tape.gradient(loss, trainable_variables)
+
+        optimizer.apply_gradients(zip(gradients, trainable_variables))
+
+    return loss, total_loss
+
+
+EPOCHS = 20
+
+def train():
+    for epoch in range(start_epoch, EPOCHS):
+        start = time.time()
+        total_loss = 0
+
+        for (batch, (img_tensor, target)) in enumerate(dataset):
+            batch_loss, t_loss = train_step(img_tensor, target)
+            total_loss += t_loss
+
+            if batch % 100 == 0:
+                print ('Epoch {} Batch {} Loss {:.4f}'.format(
+                    epoch + 1, batch, batch_loss.numpy() / int(target.shape[1]))) 
+
+        if epoch % 5 == 0:
+            ckpt_manager.save()
+        
+        print('Epoch {} loss {:.6f}'.format(epoch + 1, total_loss / num_steps))
+        print('Time taken for one epoch {} sec\n'.format(time.time() - start))
+
+
+def evaluate(image):
+    hidden = decoder.reset_state(bs=1)
+
+    temp_input = tf.expand_dims(load_image(image)[0], 0)
+    img_tensor_val = image_features_extract_model(temp_input)
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+
+    features = encoder(img_tensor_val)
+
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+    result = []
+
+    for i in range(max_length):
+        predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
+
+        predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+        result.append(tokenizer.index_word[predicted_id])
+
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return result
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result
+
+#train()
+
+rid = np.random.randint(0, len(img_name_val))
+image = img_name_val[rid]
+real_caption = ' '.join([tokenizer.index_word[i] for i in cap_val[rid] if i not in [0]])
+result = evaluate(image)
+
+print ('Real Caption:', real_caption)
+print ('Prediction Caption:', ' '.join(result))
+
+
+image_url = 'https://tensorflow.org/images/surf.jpg'
+image_extension = image_url[-4:]
+image_path = tf.keras.utils.get_file('image'+image_extension,
+                                     origin=image_url)
+
+result = evaluate(image_path)
+print ('Prediction Caption:', ' '.join(result))
